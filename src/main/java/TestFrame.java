ecd6e8684f09d71518ac0bcc49c1bf5f7e9bac6d
Anton Tobolkin <tobantal@gmail.com>
2018-08-08 17:44:50 +0700

    Sample application with JSplitPane

--- src/main/java/TestFrame.java
+++ src/main/java/TestFrame.java
@@ -3,39 +3,66 @@ import javax.swing.*;
 
 class TestFrame extends JFrame {
 
-    private JScrollPane scrollPane;
-    private JPanel topPanel;
-    private JLabel label;
+    private JSplitPane splitPaneV;
+    private JSplitPane splitPaneH;
+    private JPanel panel1;
+    private JPanel panel2;
+    private JPanel panel3;
 
     public TestFrame() {
-        setup();
-        addImage("/Users/anton_tobolkin/IdeaProjects/SimpleJFrameApp/src/main/java/image.gif");
-        createScrollPane();
-    }
-
-    void setup() {
-        setTitle( "Tabbed Pane Application" );
-        setSize( 300, 200 );
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle( "Split Pane Application" );
         setBackground( Color.gray );
-        topPanel = new JPanel();
+        JPanel topPanel = new JPanel();
         topPanel.setLayout( new BorderLayout() );
         getContentPane().add( topPanel );
+
+        // Create the panels
+        createPanel1();
+        createPanel2();
+        createPanel3();
+
+        // Create a splitter pane
+        splitPaneV = new JSplitPane( JSplitPane.VERTICAL_SPLIT );
+        topPanel.add( splitPaneV, BorderLayout.CENTER );
+        splitPaneH = new JSplitPane( JSplitPane.HORIZONTAL_SPLIT );
+        splitPaneH.setLeftComponent( panel1 );
+        splitPaneH.setRightComponent( panel2 );
+        splitPaneV.setLeftComponent( splitPaneH );
+        splitPaneV.setRightComponent( panel3 );
     }
 
-    void addImage(String file) {
-        Icon image = new ImageIcon( file );
-        label = new JLabel( image );
+    public void createPanel1() {
+        panel1 = new JPanel();
+        panel1.setLayout( new BorderLayout() );
+
+        // Add some buttons
+        panel1.add( new JButton( "North" ), BorderLayout.NORTH );
+        panel1.add( new JButton( "South" ), BorderLayout.SOUTH );
+        panel1.add( new JButton( "East" ), BorderLayout.EAST );
+        panel1.add( new JButton( "West" ), BorderLayout.WEST );
+        panel1.add( new JButton( "Center" ), BorderLayout.CENTER );
     }
 
-    void createScrollPane() {
-        scrollPane = new JScrollPane();
-        scrollPane.getViewport().add( label );
-        topPanel.add( scrollPane, BorderLayout.CENTER );
+    public void createPanel2() {
+        panel2 = new JPanel();
+        panel2.setLayout( new FlowLayout() );
+        panel2.add( new JButton( "Button 1" ) );
+        panel2.add( new JButton( "Button 2" ) );
+        panel2.add( new JButton( "Button 3" ) );
+    }
+
+    public void createPanel3() {
+        panel3 = new JPanel();
+        panel3.setLayout( new BorderLayout() );
+        panel3.setPreferredSize( new Dimension( 400, 100 ) );
+        panel3.setMinimumSize( new Dimension( 100, 50 ) );
+        panel3.add( new JLabel( "Notes:" ), BorderLayout.NORTH );
+        panel3.add( new JTextArea(), BorderLayout.CENTER );
     }
 
     public static void main( String args[] ) {
         TestFrame mainFrame = new TestFrame();
-        mainFrame.setVisible( true );
+        mainFrame.pack();
+        mainFrame.setVisible(true);
     }
 }
